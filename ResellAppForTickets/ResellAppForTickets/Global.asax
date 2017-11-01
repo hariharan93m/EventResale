698d04fe58b8c6b4a92f8b5305f971189a32c132
Hariharan <hariharan93m@gmail.com>
2017-11-01 12:21:04 +0000

    Initial commit

---
+++ ResellAppForTickets/ResellAppForTickets/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ResellAppForTickets.MvcApplication" Language="C#" %>
---
+++ ResellAppForTickets/ResellAppForTickets/Models/LoginDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResellAppForTickets.Models
+{
+    public class LoginDetails
+    {
+
+        // Create Properties for Login details
+
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
